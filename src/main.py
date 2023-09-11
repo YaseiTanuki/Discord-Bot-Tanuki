f8bf8b1246b6bbb8a7c0e2b067796517ed5fb64d
タヌキ <Tanuki1707@gmail.com>
2023-09-11 22:53:01 +0700

    load member cog

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ bot = commands.Bot(command_prefix="ta! ", intents=intents)
 @bot.event
 async def on_ready():
     await bot.load_extension('cogs.text-related.ManageMessages')
+    await bot.load_extension('cogs.member.ManageMember')
     print("Tanuki is up!!")
 
 # Start bot (must be the last function to call)
