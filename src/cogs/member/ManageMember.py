c1310b4ec4f33aa22788baeeb8120b6a5b3f65c7
タヌキ <Tanuki1707@gmail.com>
2023-09-13 23:00:23 +0700

    add set role method

--- src/cogs/member/ManageMember.py
+++ src/cogs/member/ManageMember.py
@@ -15,9 +15,23 @@ class ManageMember(commands.Cog):
     async def sayonara(seft, ctx, user: discord.Member): #kick member
         if ctx.message.author.top_role > user.top_role:
             await user.kick()
-            print(f'{user.name} is gone!')
+            await ctx.send(f'{user.name} is gone!')
         else:
-            print("You don't have permission!")
+            await ctx.send("You don't have permission!")
+
+    @commands.command()
+    async def you_are_now(seft, ctx, role: discord.Role, user:  discord.Member):
+        print(role)
+        able_to_set_role = os.getenv('ABLE_TO_SET_ROLE')
+        print(able_to_set_role)
+        if(role.name in able_to_set_role):
+            if discord.utils.get(ctx.guild.roles, name = role.name):
+                await user.add_roles(role)
+                await ctx.send(f"{user} is now {role}")
+            else:
+                await ctx.send("Role not exist!")
+        else:
+            await ctx.send("You don't have permission!")
 
 # For making extension
 async def setup(bot):
